99ced6de54e309360880bd00317935ad46879080
Aparajita <aparajita@aparajita.com>
2026-02-05 23:18:41 -0800

    fix: update xcode project so it can build

---
+++ ios/PluginTests/PluginTests.swift
@@ -0,0 +1,17 @@
+//
+//  PluginTests.swift
+//  PluginTests
+//
+//  Created by Aparajita on 2/5/26.
+//  Copyright © 2026 Max Lynch. All rights reserved.
+//
+
+import Testing
+
+struct PluginTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
