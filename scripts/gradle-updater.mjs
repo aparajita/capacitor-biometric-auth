eb1565812c1e2aa02852e6d594b7e1a79c155eea
Aparajita <aparajita@aparajita.com>
2026-02-05 21:49:10 -0800

    chore: make android version updater more robust

--- scripts/gradle-updater.mjs
+++ scripts/gradle-updater.mjs
@@ -1,18 +1,21 @@
-export function readVersion(contents) {
-  const versionMatch = contents.match(/versionName\s+"([\d.]+)"/u)
+const versionNameRegex = /versionName\s*=\s*"([\d.]+)"/u
+const versionCodeRegex = /versionCode\s*=\s*(\d+)/u
 
-  if (versionMatch) {
-    return versionMatch[1]
+export function readVersion(contents) {
+  const versionNameMatch = contents.match(versionNameRegex)
+  const versionCodeMatch = contents.match(versionCodeRegex)
+
+  if (versionNameMatch && versionCodeMatch) {
+    return versionNameMatch[1]
   }
 
-  throw new Error('Version not found in the provided contents.')
+  throw new Error(
+    'versionName and/or versionCode not found in the provided contents.',
+  )
 }
 
 export function writeVersion(contents, version) {
   return contents
-    .replace(/versionName\s+"[\d.]+"/u, `versionName "${version}"`)
-    .replace(/versionCode\s+\d+/u, (match) => {
-      const currentCode = Number.parseInt(match.match(/\d+/u)[0], 10)
-      return `versionCode ${currentCode + 1}`
-    })
+    .replace(versionNameRegex, `versionName = "${version}"`)
+    .replace(versionCodeRegex, (match, versionCode) => `versionCode = ${Number.parseInt(versionCode, 10) + 1}`)
 }
